2c4f1086aa23f555bb326606ec83097c772b6c5f
Michael Dowling <michael.dowling@minderupt.com>
2011-10-15 10:06:01 -0700

    sfdsdf

--- src/test/java/spectacular/data/model/FixtureInventoryTest.java
+++ src/test/java/spectacular/data/model/FixtureInventoryTest.java
@@ -30,4 +30,31 @@ public class FixtureInventoryTest {
     }
 
 
+    @Test
+    public void testCanMatchTextWithRegexGroups() throws Exception {
+
+        Closure testerClosure = mock(Closure.class);
+        when(testerClosure.toString()).thenReturn("tester");
+
+        Closure devClosure = mock(Closure.class);
+        when(devClosure.toString()).thenReturn("developer");
+
+        FixtureInventory inventory = new FixtureInventory();
+        inventory.addFixture("hello my (.*?) is tester", testerClosure);
+        inventory.addFixture("^(.*?) my (.*?) is developer", devClosure);
+
+        Closure tester = inventory.findClosureForText("hello my name is tester");
+        assertNotNull(tester);
+        assertEquals("tester", tester.toString());
+
+        Closure developer = inventory.findClosureForText("hello my name is developer");
+        assertNotNull(developer);
+        assertEquals("developer", developer.toString());
+
+
+
+
+    }
+
+
 }
